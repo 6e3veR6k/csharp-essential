657b09b4ba2dab1e4c7b5b31553ff42d9f41ca89
Matvey Sokolov <bezyn4uk@hotmail.com>
2019-11-08 21:01:03 +0200

    Task4 and Task5

---
+++ CsharpCourse/Task4/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+//Создайте интерфейсы IInt и IInt<Y>.
+//В теле интерфейса IInt пропишите метод без возвращаемого значения и с сигнатурой ShowClassType(), 
+
+//В теле интерфейса IInt<Y> создайте абстрактное автосвойство типа Y и метод без возвращаемого значения с сигнатурой ShowPropType().
+
+//Создайте класс Class1, реализующий интерфейс IInt, создайте класс Class2, реализующий оба интерфейса.
+//Создайте экземпляры классов Class1 и Class2 в методе Main и продемонстрируйте на них работу созданных методов.
+
+
+namespace Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 class1 = new Class1();
+            class1.ShowClassType();
+
+            Class2<decimal> class2 = new Class2<decimal>();
+            class2.ShowClassType();
+            class2.ShowPropType();
+        }
+    }
+
+    public interface IInt 
+    {
+        void ShowClassType();
+    }
+
+    public interface IInt<Y> 
+    {
+        abstract Y GetY { get; set; }
+
+        void ShowPropType();
+    }
+
+    public class Class1 : IInt
+    {
+        public void ShowClassType()
+        {
+
+            Console.WriteLine($"Type - {typeof(Class1)}");
+            Console.WriteLine($"Type - {this.GetType()}");
+        }
+    }
+
+    public class Class2<Y> : IInt, IInt<Y>
+    {
+        private Y item;
+
+        public Y GetY 
+        { 
+            get => item;
+            set => item = value; 
+        }
+
+        public void ShowClassType()
+        {
+            Console.WriteLine($"Class2 - {this.GetType()}");
+            Console.WriteLine($"Class2 - {typeof(Class2<Y>)}");
+        }
+
+        public void ShowPropType()
+        {
+            Console.WriteLine($"Class2 - {item.GetType()}");
+        }
+    }
+}
---
+++ CsharpCourse/Task5/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+/*
+ * Создайте интерфейсы IInt, IInt<Y> и IInt3<DateTime>.
+ * В теле интерфейса IInt создайте метод без возвращаемого значения и с сигнатурой ShowCurentData().
+ * В теле интерфейса IInt<Y> создайте 2 абстрактных автосвойства типа Y и абстрактный метод SetPropsData(Y a, Y b). 
+ * В теле интерфейса IInt3<DateTime> создайте абстрактное автосвойство типа DateTime пропишите метод SetCurentDateTime(). 
+ * Создайте параметризированный класс Class1 реализующий все интерфейсы и с параметром, ограниченным только ссылочными типами. 
+ * Продемонстрируйте работу методов на экземпляре класса Class1.
+*/
+
+namespace Task5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IInt tr = new Class1<string>();
+            tr.ShowCurentData();
+
+
+            IInt<string> trS = tr as IInt<string>;
+            trS.SetPropsData("First argument", "Second argument");
+            Console.WriteLine(trS.First);
+            Console.WriteLine(trS.Second);
+        }
+    }
+
+
+    public interface IInt 
+    { 
+        void ShowCurentData(); 
+    }
+
+
+    public interface IInt<Y> 
+    {
+        Y First { get; set; }
+        Y Second { get; set; }
+
+        void SetPropsData(Y a, Y b);
+    }
+
+    public interface IInt2<DateTime> 
+    {
+        DateTime Date { get; set; }
+        void SetCurentDateTime();
+    }
+
+    public class Class1<Y> : IInt, IInt<Y>, IInt2<DateTime> where Y : class
+    {
+        private Y first = default(Y);
+        private Y second = default(Y);
+        public Y First { get => first; set => first = value; }
+        public Y Second { get => second; set => second = value; }
+
+
+        private DateTime date = DateTime.MinValue;
+
+        public DateTime Date { get => date; set => date = value; }
+
+
+
+
+        public void SetCurentDateTime()
+        {
+            date = DateTime.Now;
+        }
+
+        public void SetPropsData(Y a, Y b)
+        {
+            First = a;
+            Second = b;
+        }
+
+        public void ShowCurentData()
+        {
+            Console.WriteLine($"CurentData => First: {First?.ToString()}, Second: {Second?.ToString()}");
+        }
+    }
+
+}

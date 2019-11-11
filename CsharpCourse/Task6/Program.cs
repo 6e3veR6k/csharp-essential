32b772f793b0df5b9a42b18d9a18471145bf43ce
Matvey Sokolov <bezyn4uk@hotmail.com>
2019-11-11 19:08:17 +0200

    Add new empty Tasks

---
+++ CsharpCourse/Task6/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+/*
+ * Создайте словарь типа Dictionary, закрытый двумя строками.
+ * Напишите программу, в которой пользователь вводит по 3 слова 
+ * в переводе на русском и английском языках и тем самым заполняет словарь типа Dictionary.
+ * Создайте метод, принимающий коллекцию нашего типа и запрашивающий 
+ * пользователя ввести слово на том языке, на котором внесены ключи чтобы проверить есть ли такое значение. 
+*/
+
+
+namespace Task6
+{
+    class Program
+    {
+        public static Dictionary<string, string> dictionary = new Dictionary<string, string>();
+        static void Main(string[] args)
+        {
+            Console.Write("Enter new word in english: ");
+            string wordInEnglish = Console.ReadLine();
+            Console.Write($"Enter russian translate for {wordInEnglish}: ");
+            string wordInRussian = Console.ReadLine();
+        }
+    }
+}

1e6b61e7b388a701d08131a92d26a56ca5f0f34b
Matvey Sokolov <bezyn4uk@hotmail.com>
2019-11-13 14:10:46 +0200

    Add Events

--- CsharpCourse/Events1/Program.cs
+++ CsharpCourse/Events1/Program.cs
@@ -1,20 +1,52 @@
 ﻿using System;
 
+
+/*
+ * Создайте класс  Human. Создайте делегат, который может быть проассоциирован с методом, 
+ * который ничего не возвращает и не принимает. В теле класса создайте событие JumpE, в соответствии с делегатом. 
+ * Создайте защищенный механизм доступа к событию для добавления и удаления методов-обработчиков. 
+ * Создайте метод Jump, выводящий на экран слово «прыгаю».
+ * Создайте экземпляр Human в методе Main, подпишите метод Jump на событие JumpE.
+ * Вызовите возникновение события JumpE.
+ * 
+ */
+
+
 namespace Events1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Human human = new Human();
+
+            human.JumpE += human.Jump;
+
+            human.StartGame();
         }
     }
 
-    class B
+    public delegate void DefaultDelegate();
+
+    public class Human
     {
-        void Print()
+        private event DefaultDelegate jumpE = null;
+
+        public event DefaultDelegate JumpE
         {
-            Console.WriteLine(nameof(Print)); 
+            add { jumpE += value; }
+            remove { jumpE -= value; }
+        }
+
+        public void Jump()
+        {
+            Console.WriteLine("Jumping...");
+        }
+
+        public void StartGame()
+        {
+            Console.WriteLine("Game start!");
+            jumpE();
         }
     }
 }
---
+++ CsharpCourse/Events2/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+
+/*
+ * Создайте класс OrderEventArgs, унаследуйте его от класса EventArgs (базового класса для определения классов-аргументов событий).
+ * В теле класса создайте строковое автосвойство Order.Создайте 2 класса – Owner и NotOwner.
+ * В теле каждого из этих классов создайте:
+ *      Событие типа EventHandler, закрытого OrderEventArgs и с именем OrderToDog.
+ *      Создайте метод GiveAnOrder, который принимает строку, создаёт экземпляр OrderEventArgs, 
+ *      помещает в него строку-аргумент метода и вызывает метод Invoke на событии. 
+ * Создайте класс Dog. В теле класса создайте: 
+ *      Строковое автосвойство Order;
+ *      Метод OrderHandler – метод обработчик события, принимающий два параметра
+ *          типа object (с именем sender) и типа OrderEventArgs (с именем e).
+ * Если источник события – экземпляр класса Owner:
+ * 1.	Выведите на экран строковое значение команды, полученное из OrderEventArgs ("Я выполняю команду  {e.Order}") .
+ * 2.	Присвойте свойству Order строковое значение команды. Если источник события не Owner – выведите на экран строку «Подозрительно смотрю на постороннего».
+ * В теле метода Main создайте экземпляр класса Dog. Создайте экземпляры классов Owner и NotOwner 
+ * и подпишите на события этих классов метод OrderHandler.
+ * Вызовите события экземпляров Owner и NotOwner.
+ * Выведите на экран значение свойства Order экземпляра Dog.
+
+ */
+
+namespace Events2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog dog = new Dog();
+            Owner owner = new Owner();
+            NotOwner notOwner = new NotOwner();
+
+            owner.OrderToDog += dog.OrderHandler;
+            notOwner.OrderToDog += dog.OrderHandler;
+
+            owner.GiveAnOrder("Fass");
+            notOwner.GiveAnOrder("Foo");
+
+            Console.WriteLine($"Dog is doing => {dog.Order}");
+        }
+    }
+
+    public class OrderEventArgs : EventArgs
+    {
+        public string Order { get; set; }
+
+        public OrderEventArgs()
+        {
+
+        }
+
+        public OrderEventArgs(string order)
+        {
+            Order = order;
+        }
+
+    }
+
+    public class Owner
+    {
+        private event EventHandler<OrderEventArgs> orderToDog = null;
+
+        public event EventHandler<OrderEventArgs> OrderToDog
+        {
+            add { orderToDog += value; }
+            remove { orderToDog -= value; }
+        }
+
+        public void GiveAnOrder(string someText)
+        {
+            OrderEventArgs order = new OrderEventArgs(someText);
+            orderToDog(this, order);
+        }
+    }
+
+    public class NotOwner
+    {
+        private event EventHandler<OrderEventArgs> orderToDog = null;
+
+        public event EventHandler<OrderEventArgs> OrderToDog
+        {
+            add { orderToDog += value; }
+            remove { orderToDog -= value; }
+        }
+
+        public void GiveAnOrder(string someText)
+        {
+            OrderEventArgs order = new OrderEventArgs(someText);
+            orderToDog(this, order);
+        }
+    }
+
+    public class Dog
+    {
+        public string Order { get; set; }
+
+
+        public void OrderHandler(object sender, OrderEventArgs e)
+        {
+            if(sender is Owner)
+            {
+                var orderOwner = sender as Owner;
+                Console.WriteLine($"Я выполняю команду  {e.Order}");
+                Order = e.Order;
+            }
+
+            if(sender is NotOwner)
+            {
+                Console.WriteLine("Подозрительно смотрю на постороннего");
+            }
+        }
+    }
+}
